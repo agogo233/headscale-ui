d3094de5810f4b08bd78218bb0cfcc53a9b0f1b4
agogo233 <276149387+agogo233@users.noreply.github.com>
2026-06-24 00:44:39 +0000

    feat: add delete pre-auth key button to key list

--- src/lib/users/UserCard/PreAuthKeys.svelte
+++ src/lib/users/UserCard/PreAuthKeys.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { getPreauthKeys, expirePreAuthKey } from '$lib/common/apiFunctions.svelte';
+	import { getPreauthKeys, expirePreAuthKey, deletePreAuthKey } from '$lib/common/apiFunctions.svelte';
 	import { PreAuthKey, User } from '$lib/common/classes';
 	import { alertStore, preAuthHideStore } from '$lib/common/stores';
 	import NewPreAuthKey from './PreAuthKeys/NewPreAuthKey.svelte';
@@ -19,6 +19,16 @@
 				$alertStore = error;
 			});
 
+	}
+
+	function deletePreAuthKeyAction(preAuthKeyID: string) {
+		deletePreAuthKey(preAuthKeyID)
+			.then(() => {
+				getPreauthKeysAction();
+			})
+			.catch((error) => {
+				$alertStore = error;
+			});
 	}
 
 	function getPreauthKeysAction() {
@@ -113,6 +123,12 @@
 									</svg></button
 								>
 								{/if}
+								<!-- delete button -->
+								<button class="mr-2 text-error" on:click={() => {deletePreAuthKeyAction(key.id)}}
+									><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+									</svg></button
+								>
 							</td>
 					</tr>
 				{/each}
